3eab97a4f5605ce193b8ff0caf5c68cbe65bb65b
fixerboy2009 <56456095+fixerboy2009@users.noreply.github.com>
2021-12-21 02:02:18 -0500

    Add files via upload

---
+++ fs/lolers.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo lolers
+echo lol this is useless
+echo just a test for fs
+echo why are you here?
+
+set /P why=why? 
+echo oh ok
+echo your here because %why%?
+
+set /P yorno=true or false: 
+
+echo the question above wasted your time :D
